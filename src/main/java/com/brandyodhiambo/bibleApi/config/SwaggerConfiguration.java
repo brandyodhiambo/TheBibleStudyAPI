163b68c317198dddc3e9b67eb52c941c0ebd77a4
brandyodhiambo <brandyariana99@gmail.com>
2025-03-30 20:17:46 +0300

    add security configuration to Swagger; implement JWT bearer token support

--- src/main/java/com/brandyodhiambo/bibleApi/config/SwaggerConfiguration.java
+++ src/main/java/com/brandyodhiambo/bibleApi/config/SwaggerConfiguration.java
@@ -1,9 +1,13 @@
 package com.brandyodhiambo.bibleApi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import java.util.Collections;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,7 +37,20 @@ public class SwaggerConfiguration {
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org"))
                         .contact(contact)
+                )
+                .components(
+                    new Components()
+                        .addSecuritySchemes(
+                            "bearer-key",
+                            new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(
+                    new SecurityRequirement()
+                        .addList("bearer-key", Collections.emptyList())
                 );
     }
 }
-

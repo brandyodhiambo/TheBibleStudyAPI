3e3a13262c11cf39f078e875e0a8e4dd90c229ce
brandyodhiambo <brandyariana99@gmail.com>
2025-03-30 20:30:25 +0300

    add group management features; refactor CreateGroupRequest and UpdateGroupRequest to use MeetingTime DTO

--- src/main/java/com/brandyodhiambo/bibleApi/feature/groupmgt/models/dto/CreateGroupRequest.java
+++ src/main/java/com/brandyodhiambo/bibleApi/feature/groupmgt/models/dto/CreateGroupRequest.java
@@ -7,22 +7,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalTime;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateGroupRequest {
-    
+
     @NotBlank(message = "Group name is required")
     private String name;
-    
+
     private String description;
-    
+
     private String location;
-    
-    private LocalTime meetingTime;
-    
+
+    private MeetingTime meetingTime;
+
     @NotNull(message = "Group type is required")
     private GroupType type;
 }
